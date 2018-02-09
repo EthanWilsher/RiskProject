c5d78d513b0f6f4ea84acda6aabd425e6f08dc17
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-02-09 09:23:19 -0700

    Setup all my methods

--- src/risk/view/RiskPanel.java
+++ src/risk/view/RiskPanel.java
@@ -7,10 +7,48 @@ import java.awt.*;
 public class RiskPanel extends JPanel
 {
 	private RiskController appController;
-	private JButton executButton;
+	private JButton executeButton;
 	private JTextArea convoArea;
 	private JTextField commandField;
 	private SpringLayout appLayout;
 	private JLabel commandLabel;
+	
+	
+	
+	public RiskPanel(RiskController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		convoArea = new JTextArea(10, 15);
+		commandField = new JTextField(20);
+		commandLabel = new JLabel("Type here to command your troops");
+		appLayout = new SpringLayout();
+		executeButton = new JButton("Execute");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground( new Color (158, 117, 46));
+		this.setLayout(appLayout);
+	
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 
 }
